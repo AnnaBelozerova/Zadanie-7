27db45d9b2175ce50397047ff7c5ca3659ed2411
Анна Белозерова <bel.denisovna@gmail.com>
2021-10-09 19:49:25 +0300

    Правки

---
+++ Задание 7.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7._2
+{
+    class Program
+    {   //Найти объем и площадь куба
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину стороны куба: ");
+            byte a = Convert.ToByte(Console.ReadLine());
+            double V, S;
+            VolumeSquare(a, out V, out S);
+            Console.WriteLine("Объем куба равен {0:f2}\nПлощадь поверхности куба равна {1:f2}", V, S);
+            Console.ReadKey();        
+        }
+        static void VolumeSquare (byte a, out double V, out double S)
+        {
+            V = Math.Pow(a, 3);
+            S = 6 * Math.Pow(a, 2);
+        }
+    }
+}
---
+++ Задание 7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7
+{
+    class Program
+    {   //Сравнить площади треугольников
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого тругольника:");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длины сторон второго тругольника:");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+
+            if (SquareTriangle(a1, b1, c1) == SquareTriangle(a2, b2, c2))
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+            else
+            {
+                Console.WriteLine((SquareTriangle(a1, b1, c1) > SquareTriangle(a2, b2, c2)) ? "Площадь первого треугольника больше и равна" : "Площадь второго треугольника больше");
+            }            
+            Console.ReadKey();
+        }
+        static double SquareTriangle (int a, int b, int c)
+        {
+            double p = (a + b + c) / 2;
+            double square = Math.Sqrt(p*(p-a) * (p - b) * (p - c));
+            return square;
+        }
+
+    }
+}
