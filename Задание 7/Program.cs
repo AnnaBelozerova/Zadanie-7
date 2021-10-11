4e832efcd6ebc2ebd35d6cb0de5ae61075c937ac
Анна Белозерова <bel.denisovna@gmail.com>
2021-10-11 13:27:24 +0300

    pravky

--- Задание 7/Program.cs
+++ Задание 7/Program.cs
@@ -19,21 +19,33 @@ namespace Задание_7
             int b2 = Convert.ToInt32(Console.ReadLine());
             int c2 = Convert.ToInt32(Console.ReadLine());
 
-            if (SquareTriangle(a1, b1, c1) == SquareTriangle(a2, b2, c2))
+            if (SquareExists(a1,b1,c1)&&SquareExists(a2,b2,c2))
             {
-                Console.WriteLine("Площади треугольников равны");
+                if (SquareTriangle(a1, b1, c1) == SquareTriangle(a2, b2, c2))
+                {
+                    Console.WriteLine("Площади треугольников равны");
+                }
+                else
+                {
+                    Console.WriteLine((SquareTriangle(a1, b1, c1) > SquareTriangle(a2, b2, c2)) ? "Площадь первого треугольника больше" : "Площадь второго треугольника больше");
+                }
             }
             else
             {
-                Console.WriteLine((SquareTriangle(a1, b1, c1) > SquareTriangle(a2, b2, c2)) ? "Площадь первого треугольника больше и равна" : "Площадь второго треугольника больше");
+                Console.WriteLine((SquareExists(a1, b1, c1) ? "Второй треугольник не существует" : "Первый треугольник не существует"));
             }            
             Console.ReadKey();
         }
-        static double SquareTriangle (int a, int b, int c)
+        static double SquareTriangle(int a, int b, int c)
         {
             double p = (a + b + c) / 2;
-            double square = Math.Sqrt(p*(p-a) * (p - b) * (p - c));
+            double square = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
             return square;
+        }
+        static bool SquareExists(int a, int b, int c)
+        {
+            bool exists = (a + b > c && a + c > b && c + b > a);
+            return exists;
         }
 
     }
